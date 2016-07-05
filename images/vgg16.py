e3eda0e5a19e26c6f7ae42831c37cdf30375eb61
aymericdamien <aymeric.damien@gmail.com>
2016-07-04 21:02:44 -0700

    update vgg16

--- images/vgg16.py
+++ images/vgg16.py
@@ -22,33 +22,35 @@ def vgg16(placeholderX=None):
     x = tflearn.input_data(shape=[None, 224, 224, 3], name='input',
                            placeholder=placeholderX)
 
-    x = tflearn.conv_2d(x, 64, 3, activation='relu', name='conv1_1')
-    x = tflearn.conv_2d(x, 64, 3, activation='relu', name='conv1_2')
-    x = tflearn.max_pool_2d(x, 2, strides=2, name='pool1')
+    x = tflearn.conv_2d(x, 64, 3, activation='relu', scope='conv1_1')
+    x = tflearn.conv_2d(x, 64, 3, activation='relu', scope='conv1_2')
+    x = tflearn.max_pool_2d(x, 2, strides=2)
 
-    x = tflearn.conv_2d(x, 128, 3, activation='relu', name='conv2_1')
-    x = tflearn.conv_2d(x, 128, 3, activation='relu', name='conv2_2')
-    x = tflearn.max_pool_2d(x, 2, strides=2, name='pool2')
+    x = tflearn.conv_2d(x, 128, 3, activation='relu', scope='conv2_1')
+    x = tflearn.conv_2d(x, 128, 3, activation='relu', scope='conv2_2')
+    x = tflearn.max_pool_2d(x, 2, strides=2)
 
-    x = tflearn.conv_2d(x, 256, 3, activation='relu', name='conv3_1')
-    x = tflearn.conv_2d(x, 256, 3, activation='relu', name='conv3_2')
-    x = tflearn.conv_2d(x, 256, 3, activation='relu', name='conv3_3')
-    x = tflearn.max_pool_2d(x, 2, strides=2, name='pool3')
+    x = tflearn.conv_2d(x, 256, 3, activation='relu', scope='conv3_1')
+    x = tflearn.conv_2d(x, 256, 3, activation='relu', scope='conv3_2')
+    x = tflearn.conv_2d(x, 256, 3, activation='relu', scope='conv3_3')
+    x = tflearn.max_pool_2d(x, 2, strides=2)
 
-    x = tflearn.conv_2d(x, 512, 3, activation='relu', name='conv4_1')
-    x = tflearn.conv_2d(x, 512, 3, activation='relu', name='conv4_2')
-    x = tflearn.conv_2d(x, 512, 3, activation='relu', name='conv4_3')
-    x = tflearn.max_pool_2d(x, 2, strides=2, name='pool4')
+    x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv4_1')
+    x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv4_2')
+    x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv4_3')
+    x = tflearn.max_pool_2d(x, 2, strides=2)
 
-    x = tflearn.conv_2d(x, 512, 3, activation='relu', name='conv5_1')
-    x = tflearn.conv_2d(x, 512, 3, activation='relu', name='conv5_2')
-    x = tflearn.conv_2d(x, 512, 3, activation='relu', name='conv5_3')
-    x = tflearn.max_pool_2d(x, 2, strides=2, name='pool5')
+    x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv5_1')
+    x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv5_2')
+    x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv5_3')
+    x = tflearn.max_pool_2d(x, 2, strides=2)
 
-    x = tflearn.fully_connected(x, 4096, activation='relu', name='fc6')
+    x = tflearn.fully_connected(x, 4096, activation='relu', scope='fc6')
     x = tflearn.dropout(x, 0.5)
 
-    x = tflearn.fully_connected(x, 4096, activation='relu', name='fc7')
+    x = tflearn.fully_connected(x, 4096, activation='relu', scope='fc7')
     x = tflearn.dropout(x, 0.5)
+
+    x = tflearn.fully_connected(x, 4096, activation='softmax', scope='fc8')
 
     return x
