3be8a76e319f261993080dabb39e5a9448b72d21
Aymeric Damien <aymeric.damien@gmail.com>
2016-07-13 15:10:00 +0800

    Update vgg16.py

--- images/vgg16.py
+++ images/vgg16.py
@@ -24,32 +24,32 @@ def vgg16(placeholderX=None):
 
     x = tflearn.conv_2d(x, 64, 3, activation='relu', scope='conv1_1')
     x = tflearn.conv_2d(x, 64, 3, activation='relu', scope='conv1_2')
-    x = tflearn.max_pool_2d(x, 2, strides=2)
+    x = tflearn.max_pool_2d(x, 2, strides=2, name='maxpool1')
 
     x = tflearn.conv_2d(x, 128, 3, activation='relu', scope='conv2_1')
     x = tflearn.conv_2d(x, 128, 3, activation='relu', scope='conv2_2')
-    x = tflearn.max_pool_2d(x, 2, strides=2)
+    x = tflearn.max_pool_2d(x, 2, strides=2, name='maxpool2')
 
     x = tflearn.conv_2d(x, 256, 3, activation='relu', scope='conv3_1')
     x = tflearn.conv_2d(x, 256, 3, activation='relu', scope='conv3_2')
     x = tflearn.conv_2d(x, 256, 3, activation='relu', scope='conv3_3')
-    x = tflearn.max_pool_2d(x, 2, strides=2)
+    x = tflearn.max_pool_2d(x, 2, strides=2, name='maxpool3')
 
     x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv4_1')
     x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv4_2')
     x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv4_3')
-    x = tflearn.max_pool_2d(x, 2, strides=2)
+    x = tflearn.max_pool_2d(x, 2, strides=2, name='maxpool4')
 
     x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv5_1')
     x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv5_2')
     x = tflearn.conv_2d(x, 512, 3, activation='relu', scope='conv5_3')
-    x = tflearn.max_pool_2d(x, 2, strides=2)
+    x = tflearn.max_pool_2d(x, 2, strides=2, name='maxpool5')
 
     x = tflearn.fully_connected(x, 4096, activation='relu', scope='fc6')
-    x = tflearn.dropout(x, 0.5)
+    x = tflearn.dropout(x, 0.5, name='dropout1')
 
     x = tflearn.fully_connected(x, 4096, activation='relu', scope='fc7')
-    x = tflearn.dropout(x, 0.5)
+    x = tflearn.dropout(x, 0.5, name='dropout2')
 
     x = tflearn.fully_connected(x, 1000, activation='softmax', scope='fc8')
 
